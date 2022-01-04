80f5e3766f8cc5bf251f0f5f212781cbe3813054
hadhemi.sahbani <hadhemi.sahbani@gmail.com>
2022-01-04 14:59:18 +0100

    first

---
+++ src/main/java/org/sid/SecServiceApplication.java
@@ -0,0 +1,39 @@
+package org.sid;
+
+import org.sid.entities.AppRole;
+import org.sid.service.AccountService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class SecServiceApplication {
+
+  public static void main(String[] args) {
+        SpringApplication.run(SecServiceApplication.class, args);
+    }
+    
+    @Bean
+    CommandLineRunner start(AccountService accountService , AppRoleRepository roleRepo){
+        return args->{
+        	AppRole r1 =roleRepo.save(new AppRole( null ,"FOURNISSEUR"));
+            accountService.save(new AppRole(null,"USER"));
+            accountService.save(new AppRole(null,"ADMIN"));
+            Stream.of("user1","user2","user3","admin").forEach(un->{
+                accountService.saveUser(un,"1234","1234","2121212" ,"Male" ,"monjislim");
+            });
+            accountService.addRoleToUser("admin","ADMIN");
+        };
+    }
+    
+    @Bean
+    BCryptPasswordEncoder getBCPE(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
+
---
+++ src/main/java/org/sid/web/UserController.java
@@ -0,0 +1,37 @@
+package org.sid.web;
+
+import lombok.Data;
+import org.sid.entities.AppUser;
+import org.sid.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+  @Autowired
+    private AccountService accountService;//UserForm userForm
+    @PostMapping("/registerUser")
+    public AppUser registerUser(@RequestBody  UserForm userForm){
+        return  accountService.saveUser(userForm.getUsername(), userForm.getPassword(),userForm.getConfirmedPassword()
+        		    ,userForm.getPhoneNumber() ,userForm.getGender(),userForm.getAddress());
+    }
+    @PostMapping("/registerFour")
+    public AppUser registerFournisseur(@RequestBody  UserForm userForm){
+        return  accountService.saveFournisseur(userForm.getUsername(), userForm.getPassword(),userForm.getConfirmedPassword()
+        		    ,userForm.getPhoneNumber() ,userForm.getGender(),userForm.getAddress());
+    }
+    
+}
+
+@Data
+class UserForm{
+    private String username;
+    private String password;
+    private String confirmedPassword;
+    private String phoneNumber ;
+    private String gender;
+    private String address;
+    
+}
